9e4f2ff6a7a65b7b6f9c35a51def629147396c10
Alex Martinig <alexmartinig90@gmail.com>
2023-06-16 16:19:11 +0100

    added embed youtube function

--- components/editor/ToolBar/index.tsx
+++ components/editor/ToolBar/index.tsx
@@ -20,6 +20,7 @@ import {
 import Button from "./Button";
 import InsertLink from "../Link/InsertLink";
 import { linkOption } from "../Link/LinkForm";
+import EmbedYoutube from "./EmbedYoutube";
 
 interface Props {
   editor: Editor | null;
@@ -67,6 +68,10 @@ const ToolBar: FC<Props> = ({ editor }): JSX.Element | null => {
     const { commands } = editor;
     if (openInNewTab) commands.setLink({ href: url, target: "_blank" });
     else commands.setLink({ href: url });
+  };
+
+  const handleEmbedYoutube = (url: string) => {
+    editor.chain().focus().setYoutubeVideo({ src: url }).run();
   };
 
   const Head = () => {
@@ -161,9 +166,7 @@ const ToolBar: FC<Props> = ({ editor }): JSX.Element | null => {
       <div className="h-4 w-[1px] bg-secondary-dark dark:bg-secondary-light mx-8" />
 
       <div className="flex items-center space-x-3">
-        <Button>
-          <BsYoutube />
-        </Button>
+        <EmbedYoutube onSubmit={handleEmbedYoutube} />
 
         <Button>
           <BsImageFill />
