5db1667cef319f29e2101891172888105f41559d
Alex Martinig <alex.martinig@calibre.org.uk>
2023-06-19 16:37:56 +0100

    Adding images to gallery and changing scrollbar

--- components/editor/GalleryModal/index.tsx
+++ components/editor/GalleryModal/index.tsx
@@ -1,15 +1,22 @@
 import ModalContainer, { ModalProps } from "@/components/common/ModalContainer";
 import { FC } from "react";
+import Gallery from "./Gallery";
 
-interface Props extends ModalProps {
+interface Props extends ModalProps {}
 
-}
-
-const GalleryModal: FC<Props> = ({visible, onClose}): JSX.Element => {
+const GalleryModal: FC<Props> = ({ visible, onClose }): JSX.Element => {
   return (
     <ModalContainer visible={visible} onClose={onClose}>
-      <div className="bg-black p-20">
-        <button className="bg-white p-3">Click me</button>
+      <div className="max-w-4xl p-2 bg-primary-dark dark:bg-primary rounded">
+        <div className="flex">
+          {/* gallery */}
+          <div className="basis-[75%] max-h-[450px] overflow-y-auto custom-scroll-bar">
+            <Gallery />
+          </div>
+
+          {/* image selection and upload */}
+          <div className="basis-[25%]"></div>
+        </div>
       </div>
     </ModalContainer>
   );
