1fed0768d8daffe083e8fe3f92df77272330865b
Alex Martinig <alex.martinig@calibre.org.uk>
2023-07-03 16:52:09 +0100

    added text limit to meta field

--- components/editor/SeoForm.tsx
+++ components/editor/SeoForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, FC } from "react";
+import { ChangeEventHandler, FC, useState } from "react";
 import classNames from "classnames";
 
 interface Props {}
@@ -10,7 +10,7 @@ const Input: FC<{
   name?: string;
   value?: string;
   placeholder?: string;
-  label?: string
+  label?: string;
   onChange?: ChangeEventHandler<HTMLInputElement>;
 }> = ({ name, value, placeholder, label, onChange }) => {
   return (
@@ -31,20 +31,49 @@ const Input: FC<{
 };
 
 const SeoForm: FC<Props> = (props): JSX.Element => {
+  const [values, setValues] = useState({ meta: "", slug: "", tags: "" });
+
+  const handleChange: ChangeEventHandler<
+    HTMLTextAreaElement | HTMLInputElement
+  > = ({ target }) => {
+    let { name, value } = target;
+    if (name === "meta") value = value.substring(0, 150);
+    setValues({ ...values, [name]: value });
+  };
+
+  const { meta, slug, tags } = values;
+
   return (
     <div className="space-y-4">
       <h1 className="text-primary-dark dark:text-primary text-xl font-semibold">
         SEO Section
       </h1>
-      <Input name="slug" placeholder="slug-goes-here" label="Slug:" />
-      <Input name="slug" placeholder="React, Next JS" label="Tags:" />
+      <Input
+        value={slug}
+        onChange={handleChange}
+        name="slug"
+        placeholder="slug-goes-here"
+        label="Slug:"
+      />
+      <Input
+        value={tags}
+        onChange={handleChange}
+        name="tags"
+        placeholder="React, Next JS"
+        label="Tags:"
+      />
 
       <div className="relative">
         <textarea
+          name="meta"
+          value={meta}
+          onChange={handleChange}
           className={classNames(commonInput, "text-lg h-20 resize-none")}
           placeholder="Meta description"
         ></textarea>
-        <p className="absolute bottom-3 right-3 text-primary-dark dark:text-primary text-sm">0/150</p>
+        <p className="absolute bottom-3 right-3 text-primary-dark dark:text-primary text-sm">
+          {meta.length}/150
+        </p>
       </div>
     </div>
   );
